0e45b5a96d1767e2338c48ec2a00f7973b7ea0a1
Oluwatobi Kehinde <oluwatobi_kehinde@yahoo.com>
2022-07-04 14:38:36 -0700

    c code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,14 @@
 #include <unistd.h>
 /**
  * Main - Entry point for our program using betty linter
+ *
+ * Description: 'art is useful'
  * Return: 1 Always (Success)
  *
  */
 int main(void)
 {
-/* Note: You are not allowed to use either printf or puts instead we are to use write*/
+/* Note: You are not allowed to use either printf or puts*/
 char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, quote, sizeof(quote) - 1);
 return (1);
