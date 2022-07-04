32d7d10112ab617f01ad807e843a9b07ddd851d9
Oluwatobi Kehinde <oluwatobi_kehinde@yahoo.com>
2022-07-04 12:26:29 -0700

    c code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * main - entry point
  * Return: 0 after printing the function
  */
-int main (void)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-} 
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
