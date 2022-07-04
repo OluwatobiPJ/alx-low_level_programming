5ef5b2b55f7977935bc4464a1e2e6a3761adbc0e
Oluwatobi Kehinde <oluwatobi_kehinde@yahoo.com>
2022-07-04 14:34:26 -0700

    c code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,10 @@
  * Return: 1 Always (Success)
  *
  */
-int main (void)
-
+int main(void)
 {
+/* Note: You are not allowed to use either printf or puts instead we are to use write*/
 char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, quote, sizeof(quote) - 1);
-return(1);
+return (1);
 }
