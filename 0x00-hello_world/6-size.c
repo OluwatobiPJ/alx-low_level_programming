280b9ca168c84b3644e8677ff9a516cd269e21a2
Oluwatobi Kehinde <oluwatobi_kehinde@yahoo.com>
2022-07-04 12:48:49 -0700

    c code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,18 +3,17 @@
  * main Entry point
  * Return: Always 0 (success)
  */
-int main (void)
-{ 
+int main(void)
+{
 char c;
 int i;
-long int longi;
+long int lngi;
 long long int lx2_i;
 float f;
 printf("Size of a char: %lu byte(s) \n", (unsigned long)sizeof(c));
 printf("Size of an int: %lu byte(s) \n", (unsigned long)sizeof(i));
-printf("Size of a long int: %lu byte(s) \n", (unsigned long)sizeof(longi));
+printf("Size of a long int: %lu byte(s) \n", (unsigned long)sizeof(lngi));
 printf("Size of a long long int: %lu byte(s) \n", (unsigned long)sizeof(lx2_i));
 printf("Size of a float: %lu byte(s) \n", (unsigned long)sizeof(f));
-
 return (0);
 }
