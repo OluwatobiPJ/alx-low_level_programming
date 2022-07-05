4800c07d80f59ac1cefe3c0ba4212688cf78c28d
Oluwatobi Kehinde <oluwatobi_kehinde@yahoo.com>
2022-07-05 10:22:22 -0700

    loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Check description
+ *
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: Nothing.
+ */
+
+void print_alphabet(void)
+{
+int j;
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
