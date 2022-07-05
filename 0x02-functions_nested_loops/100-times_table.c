7304f6be2db580a6c8c206a72b0f72a92e25be26
Oluwatobi Kehinde <oluwatobi_kehinde@yahoo.com>
2022-07-05 14:50:15 -0700

    loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,58 @@
 #include "main.h"
 
 /**
- * print_times_table -  prints the n times table, starting with 0.
- *
- * @n: integer input
- *
- * Return: void
+ * print_times_table - prints times table for numbers from 0-14
+ * @n: An input integer value
+ * Return: Nothing
  */
 
 void print_times_table(int n)
 
 {
-int a, b, c;
-if ((n > 0) && (n < 15))
+int i, j;
+if (n > 0 && n < 15)
 {
-for (a = 0; a <= n; a++)
+for (i = 0; i <= n; i++)
 {
+_putchar('0');
+for (j = 1; j <= n; j++)
+putformat(i * j);
 _putchar('\n');
-for (b = 0; b <= n; b++)
+}
+}
+}
+
+
+/**
+ * putformat - formatted characters to output
+ * @n: number to format
+ * Return: nothing
+ */
+
+void putformat(int n)
 {
-if (b > 0)
+if (n <= 9)
+{
 _putchar(',');
-c = (a * b);
-if  ((c >= 0) && (c < 10))
-{
 _putchar(' ');
 _putchar(' ');
 _putchar(' ');
+_putchar(n + '0');
 }
-if ((c >= 10) && (c < 100))
+else if (n > 9 && n <= 99)
 {
+_putchar(',');
 _putchar(' ');
 _putchar(' ');
-_putchar((c / 10) + '0');
-_putchar((c % 10) + '0');
-}
-else if ((c >= 100) && (c < 1000))
-{
-_putchar(' ');
-_putchar((c / 100) + '0');
-_putchar((c / 10) % 10  + '0');
-_putchar((c % 10) + '0');
+_putchar(n / 10 + '0');
+_putchar(n % 10 + '0');
 }
 else
-_putchar(c + '0');
-}
-}
+{
+_putchar(',');
+_putchar(' ');
+_putchar(n / 100 + '0');
+_putchar(n / 10 % 10 + '0');
+_putchar(n % 10 + '0');
 }
 }
