f46742134c1f48b03f82f0fc498e63fd2b285d85
Oluwatobi Kehinde <oluwatobi_kehinde@yahoo.com>
2022-07-05 04:40:30 -0700

    c code

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,22 +5,24 @@
  * Return: always return 0
  */
 int main(void)
+
 {
-int ch;
-int n;
-for (ch = 48; ch <= 57; ch++)
+int p, q;
+for (p = 0; p <= 98; p++)
 {
-for (n = 48; n <= 57; n++)
+for (q = p + 1; q <= 99; q++)
 {
-putchar(ch);
-putchar(n);
-if (ch != 57 || n != 57)
-{
-putchar(44);
-putchar(32);
+putchar((p / 10) + '0');
+putchar((p % 10) + '0');
+putchar(' ');
+putchar((q / 10) + '0');
+putchar((q % 10) + '0');
+if (p == 98 && q == 99)
+continue;
+putchar(',');
+putchar(' ');
 }
 }
-}
-putchar(10);
+putchar('\n');
 return (0);
 }
