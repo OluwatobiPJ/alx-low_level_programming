360eb99f8333626d4157d4eb93b6a98a78b90b6e
Oluwatobi Kehinde <oluwatobi_kehinde@yahoo.com>
2022-07-05 10:12:23 -0700

    loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- *
  * main - Check description
  *
  * Description: It prints the word _putchar, followed by a new line.
@@ -11,16 +10,15 @@
 int main(void)
 
 {
- _putchar('_');
- _putchar('p');
- _putchar('u');
- _putchar('t');
- _putchar('c');
- _putchar('h');
- _putchar('a');
- _putchar('r');
- _putchar('\n');
- 
- return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 
 }
