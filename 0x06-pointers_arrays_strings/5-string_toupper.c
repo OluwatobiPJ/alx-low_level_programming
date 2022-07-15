f6851a8385452781d187a71b7c8056572b5ea477
Oluwatobi Kehinde <oluwatobi_kehinde@yahoo.com>
2022-07-15 02:41:29 -0700

    more arrays

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,23 +1,24 @@
 #include "main.h"
 
 /**
- * string_toupper -> converting any string to upper case
+ * string_toupper - fonction toupper
  *
- * @x: string param
+ * @a: pointer
  *
- *Return: string
+ * Return: pointer in return
  *
  */
 
-char *string_toupper(char *x)
+char *string_toupper(char *a)
 
 {
-int i = 0;
-while (x[i])
+int i;
+for (i = 0; a[i] != 0; i++)
 {
-if (x[i] >= 97 && x[i] <= 122)
-[i] = x[i] - 32;
-i++;
+if (a[i] > 90 && a[i] != 10)
+{
+a[i] = a[i] - 32;
 }
-return (x);
+}
+return (a);
 }
