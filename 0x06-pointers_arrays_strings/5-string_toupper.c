e0aa57e48e19dd969ce542f0de151310973a72f3
Oluwatobi Kehinde <oluwatobi_kehinde@yahoo.com>
2022-07-15 02:16:06 -0700

    more arrays

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper -> converting any string to upper case
+ *
+ * @x: string param
+ *
+ *Return: string
+ *
+ */
+
+char *string_toupper(char *x)
+
+{
+int i = 0;
+while (x[i])
+{
+if (x[i] >= 97 && x[i] <= 122)
+[i] = x[i] - 32;
+i++;
+}
+return (x);
+}
